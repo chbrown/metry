2384d8768fe51054bc9198746c0088aecb1119a1
Christopher Brown <io@henrian.com>
2015-12-09 15:40:32 -0600

    Move from http-enhanced to restify; add start/end query parameters to /actions endpoint

--- server.js
+++ server.js
@@ -1,14 +1,157 @@
-var http = require('http-enhanced');
-var loge_1 = require('loge');
-var indexController = require('./controllers/index');
-var server = http.createServer(function (req, res) {
-    loge_1.logger.debug('%s %s', req.method, req.url);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    indexController(req, res);
+var restify_1 = require('restify');
+var database_1 = require('./database');
+var package_json = require('./package.json');
+exports.app = restify_1.createServer();
+exports.app.use(restify_1.CORS());
+exports.app.use(restify_1.queryParser());
+// keep req.params and req.body distinct, so that we can distingush between POST and url params
+exports.app.use(restify_1.bodyParser({ mapParams: false }));
+/**
+Simple date parser, but returns null for invalid input.
+*/
+function parseDate(text) {
+    var date = new Date(text);
+    return isNaN(date.getTime()) ? null : date;
+}
+function sendCallback(res, next) {
+    return function (error, result) {
+        if (error) {
+            return next(error);
+        }
+        if (result === undefined) {
+            res.send(404, new Error('No results found'));
+            return next();
+        }
+        res.send(result);
+        return next();
+    };
+}
+/** GET /info
+Show metry package metadata
+*/
+exports.app.get('info', function (req, res, next) {
+    res.send({
+        name: package_json.name,
+        version: package_json.version,
+        description: package_json.description,
+        homepage: package_json.homepage,
+        author: package_json.author,
+        license: package_json.license,
+    });
 });
-server.on('listening', function () {
-    var address = server.address();
-    loge_1.logger.info("server listening on http://" + address.address + ":" + address.port);
+/*******************************************************************************
+                                 actions
+*******************************************************************************/
+/** GET /actions
+List all actions.
+*/
+exports.app.get('actions', function (req, res, next) {
+    var select = database_1.default.Select('distinct_action').where('deleted IS NULL');
+    var start = parseDate(req.params.start);
+    if (start) {
+        select = select.where('started > ?', start);
+    }
+    var end = parseDate(req.params.end);
+    if (end) {
+        select = select.where('ended < ?', end);
+    }
+    select.execute(sendCallback(res, next));
 });
-module.exports = server;
+/** GET /actions/new
+Generate blank action.
+*/
+exports.app.get('actions/new', function (req, res, next) {
+    res.send({ entered: new Date() });
+    next();
+});
+/** POST /actions
+    POST /actions/
+    POST /actions/:action_id
+Create / update action.
+It's basically the same thing since the `action` table is immutable.
+*/
+exports.app.post('actions/:action_id', function (req, res, next) {
+    // the action_id supplied in the URL should override the payload, even if undefined
+    // if it's the empty string, use undefined instead
+    var action_id = req.params.action_id || undefined;
+    var _a = req.body, actiontype_id = _a.actiontype_id, started = _a.started, ended = _a.ended, deleted = _a.deleted;
+    database_1.default.InsertOne('action')
+        .set({ action_id: action_id, actiontype_id: actiontype_id, started: started, ended: ended, deleted: deleted })
+        .returning('*')
+        .execute(sendCallback(res, next)); // HTTP 201
+});
+/** GET /actions/:action_id
+Show existing action.
+*/
+exports.app.get('actions/:action_id', function (req, res, next) {
+    database_1.default.SelectOne('action')
+        .whereEqual({ action_id: req.params.action_id })
+        .orderBy('entered DESC')
+        .where('deleted IS NULL')
+        .execute(sendCallback(res, next));
+});
+/** DELETE /actions/:action_id
+Delete existing action.
+*/
+exports.app.del('actions/:action_id', function (req, res, next) {
+    database_1.default.Insert('action')
+        .set({ action_id: req.params.action_id, deleted: new Date() })
+        .execute(sendCallback(res, next)); // HTTP 204
+});
+/*******************************************************************************
+                                actiontypes
+*******************************************************************************/
+/** GET /actiontypes
+List all actiontypes.
+*/
+exports.app.get('actiontypes', function (req, res, next) {
+    database_1.default.Select('distinct_actiontype')
+        .where('deleted IS NULL')
+        .orderBy('view_order ASC, actiontype_id ASC')
+        .execute(sendCallback(res, next));
+});
+/** GET /actiontypes/new
+Generate blank actiontype.
+*/
+exports.app.get('actiontypes/new', function (req, res, next) {
+    res.send({ entered: new Date() });
+    next();
+});
+/** POST /actiontypes
+    POST /actiontypes/
+    POST /actiontypes/:actiontype_id
+Create / update actiontypes.
+It's basically the same thing since the `actiontype` table is immutable.
+*/
+exports.app.post('actiontypes/:actiontype_id', function (req, res, next) {
+    // if actiontype_id in the url is the empty string, use undefined instead
+    var actiontype_id = req.params.actiontype_id || undefined;
+    var _a = req.body, name = _a.name, view_order = _a.view_order, archived = _a.archived, deleted = _a.deleted;
+    database_1.default.InsertOne('actiontype')
+        .set({ actiontype_id: actiontype_id, name: name, view_order: view_order, archived: archived, deleted: deleted })
+        .returning('*')
+        .execute(sendCallback(res, next)); // HTTP 201
+});
+/** GET /actiontypes/:actiontype_id
+Show existing actiontype.
+*/
+exports.app.get('actiontypes/:actiontype_id', function (req, res, next) {
+    database_1.default.SelectOne('distinct_actiontype')
+        .whereEqual({ actiontype_id: req.params.actiontype_id })
+        .where('deleted IS NULL')
+        .execute(sendCallback(res, next));
+});
+/** DELETE /actiontypes/:actiontype_id
+Delete existing actiontype.
+*/
+exports.app.del('actiontypes/:actiontype_id', function (req, res, next) {
+    database_1.default.Insert('actiontype')
+        .set({ actiontype_id: req.params.actiontype_id, deleted: new Date() })
+        .execute(sendCallback(res, next)); // HTTP 204
+});
+exports.app.on('listening', function () {
+    var address = exports.app.address();
+    console.log("server listening on http://" + address.address + ":" + address.port);
+});
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = exports.app;
--- server.ts
+++ server.ts
@@ -1,17 +1,172 @@
-var http = require('http-enhanced');
-import {logger} from 'loge';
+import {join} from 'path';
+import {createServer, bodyParser, queryParser, CORS, Response, Next} from 'restify';
+import db from './database';
+var package_json = require('./package.json');
 
-var indexController = require('./controllers/index');
+export const app = createServer();
 
-var server = http.createServer((req, res) => {
-  logger.debug('%s %s', req.method, req.url);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  indexController(req, res);
+app.use(CORS());
+app.use(queryParser());
+// keep req.params and req.body distinct, so that we can distingush between POST and url params
+app.use(bodyParser({mapParams: false}));
+
+/**
+Simple date parser, but returns null for invalid input.
+*/
+function parseDate(text: string): Date {
+  var date = new Date(text);
+  return isNaN(date.getTime()) ? null : date;
+}
+
+function sendCallback(res: Response, next: Next) {
+  return (error: Error, result?: any) => {
+    if (error) {
+      return next(error);
+    }
+    if (result === undefined) {
+      res.send(404, new Error('No results found'));
+      return next();
+    }
+    res.send(result);
+    return next();
+  }
+}
+
+/** GET /info
+Show metry package metadata
+*/
+app.get('info', (req, res, next) => {
+  res.send({
+    name: package_json.name,
+    version: package_json.version,
+    description: package_json.description,
+    homepage: package_json.homepage,
+    author: package_json.author,
+    license: package_json.license,
+  });
 });
-server.on('listening', () => {
-  var address = server.address();
-  logger.info(`server listening on http://${address.address}:${address.port}`);
+
+/*******************************************************************************
+                                 actions
+*******************************************************************************/
+/** GET /actions
+List all actions.
+*/
+app.get('actions', (req, res, next) => {
+  let select = db.Select('distinct_action').where('deleted IS NULL');
+
+  let start = parseDate(req.params.start);
+  if (start) {
+    select = select.where('started > ?', start);
+  }
+  let end = parseDate(req.params.end);
+  if (end) {
+    select = select.where('ended < ?', end);
+  }
+
+  select.execute(sendCallback(res, next));
+});
+/** GET /actions/new
+Generate blank action.
+*/
+app.get('actions/new', (req, res, next) => {
+  res.send({entered: new Date()});
+  next();
+});
+/** POST /actions
+    POST /actions/
+    POST /actions/:action_id
+Create / update action.
+It's basically the same thing since the `action` table is immutable.
+*/
+app.post('actions/:action_id', (req, res, next) => {
+  // the action_id supplied in the URL should override the payload, even if undefined
+  // if it's the empty string, use undefined instead
+  let action_id = req.params.action_id || undefined;
+  let {actiontype_id, started, ended, deleted} = req.body;
+
+  db.InsertOne('action')
+  .set({action_id, actiontype_id, started, ended, deleted})
+  .returning('*')
+  .execute(sendCallback(res, next)); // HTTP 201
+});
+/** GET /actions/:action_id
+Show existing action.
+*/
+app.get('actions/:action_id', (req, res, next) => {
+  db.SelectOne('action')
+  .whereEqual({action_id: req.params.action_id})
+  .orderBy('entered DESC')
+  .where('deleted IS NULL')
+  .execute(sendCallback(res, next));
+});
+/** DELETE /actions/:action_id
+Delete existing action.
+*/
+app.del('actions/:action_id', (req, res, next) => {
+  db.Insert('action')
+  .set({action_id: req.params.action_id, deleted: new Date()})
+  .execute(sendCallback(res, next)); // HTTP 204
 });
 
-export = server;
+
+/*******************************************************************************
+                                actiontypes
+*******************************************************************************/
+
+/** GET /actiontypes
+List all actiontypes.
+*/
+app.get('actiontypes', (req, res, next) => {
+  db.Select('distinct_actiontype')
+  .where('deleted IS NULL')
+  .orderBy('view_order ASC, actiontype_id ASC')
+  .execute(sendCallback(res, next));
+});
+/** GET /actiontypes/new
+Generate blank actiontype.
+*/
+app.get('actiontypes/new', (req, res, next) => {
+  res.send({entered: new Date()});
+  next();
+});
+/** POST /actiontypes
+    POST /actiontypes/
+    POST /actiontypes/:actiontype_id
+Create / update actiontypes.
+It's basically the same thing since the `actiontype` table is immutable.
+*/
+app.post('actiontypes/:actiontype_id', (req, res, next) => {
+  // if actiontype_id in the url is the empty string, use undefined instead
+  let actiontype_id = req.params.actiontype_id || undefined;
+  let {name, view_order, archived, deleted} = req.body;
+
+  db.InsertOne('actiontype')
+  .set({actiontype_id, name, view_order, archived, deleted})
+  .returning('*')
+  .execute(sendCallback(res, next)); // HTTP 201
+});
+/** GET /actiontypes/:actiontype_id
+Show existing actiontype.
+*/
+app.get('actiontypes/:actiontype_id', (req, res, next) => {
+  db.SelectOne('distinct_actiontype')
+  .whereEqual({actiontype_id: req.params.actiontype_id})
+  .where('deleted IS NULL')
+  .execute(sendCallback(res, next));
+});
+/** DELETE /actiontypes/:actiontype_id
+Delete existing actiontype.
+*/
+app.del('actiontypes/:actiontype_id', (req, res, next) => {
+  db.Insert('actiontype')
+  .set({actiontype_id: req.params.actiontype_id, deleted: new Date()})
+  .execute(sendCallback(res, next)); // HTTP 204
+});
+
+app.on('listening', () => {
+  var address = app.address();
+  console.log(`server listening on http://${address.address}:${address.port}`);
+});
+
+export default app;
